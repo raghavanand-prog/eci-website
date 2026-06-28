968e29d2c097bef29036b2fdd5455ac66e0cb9cd
Raghav Anand <raghavanand033@gmail.com>
2026-06-28 11:11:02 +0530

    voter registration system with local storage

--- ECI/script.js
+++ ECI/script.js
@@ -65,3 +65,76 @@ document.addEventListener("DOMContentLoaded", function () {
     }
 
 });
+const form = document.getElementById("registrationForm");
+
+if (form) {
+
+    form.addEventListener("submit", (event) => {
+
+        event.preventDefault();
+
+        const name = document.getElementById("name").value;
+        const email = document.getElementById("email").value;
+        const password = document.getElementById("password").value;
+        const phone = document.getElementById("phone").value;
+        const dob = document.getElementById("dob").value;
+        const state = document.getElementById("state").value;
+        const address = document.getElementById("address").value;
+        const proof = document.getElementById("proof").files[0];
+
+        const gender = document.querySelector('input[name="gender"]:checked');
+        const terms = document.getElementById("terms").checked;
+
+        if (!gender) {
+            alert("Please select your gender.");
+            return;
+        }
+
+        if (!terms) {
+            alert("Please accept the Terms & Conditions.");
+            return;
+        }
+
+        // Create object to store all voter details
+        const voterData = {
+            name: name,
+            email: email,
+            password: password,
+            phone: phone,
+            dob: dob,
+            gender: gender.value,
+            state: state,
+            address: address,
+            proof: proof ? proof.name : "Not Uploaded"
+        };
+
+        // Save object in Local Storage
+        localStorage.setItem("voterDetails", JSON.stringify(voterData));
+        document.getElementById("successMessage").innerHTML = `
+    <div class="success-box">
+        ✅ Registration Successful!<br>
+        Thank you for registering with the Election Commission of India.
+    </div>
+`;
+
+        // Display entered data
+        const resultCard = document.getElementById("resultCard");
+
+        resultCard.innerHTML = `
+            <div class="result-card">
+                <h2>Voter Registration Details</h2>
+
+                <p><strong>Name:</strong> ${voterData.name}</p>
+                <p><strong>Email:</strong> ${voterData.email}</p>
+                <p><strong>Mobile:</strong> ${voterData.phone}</p>
+                <p><strong>Date of Birth:</strong> ${voterData.dob}</p>
+                <p><strong>Gender:</strong> ${voterData.gender}</p>
+                <p><strong>State:</strong> ${voterData.state}</p>
+                <p><strong>Address:</strong> ${voterData.address}</p>
+                <p><strong>Identity Proof:</strong> ${voterData.proof}</p>
+            </div>
+        `;
+
+    });
+
+}
